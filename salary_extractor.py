915c1f40542beed4168b1c6f3f3d920bd3e73b8d
rkinwork <rkinwork@gmail.com>
2019-08-19 22:30:10 +0300

    Make AbstractBaseStatisticsFetcher less complicated

--- salary_extractor.py
+++ salary_extractor.py
@@ -1,25 +1,9 @@
-import datetime
 import argparse
+import datetime
 
 from dotenv import load_dotenv
 
-from salary_extractor_lib import AsciiTableStatisticRenderer, SJFetcher, HHFetcher
-
-TOP_LANGUAGES = ['JavaScript',
-                 'Python',
-                 'Java',
-                 'C++',
-                 'C',
-                 'PHP',
-                 'C#',
-                 'Shell',
-                 'Go',
-                 'TypeScript',
-                 'Ruby',
-                 'Objective-C',
-                 'Swift',
-                 'Kotlin',
-                 '1С']
+from salary_extractor_lib import AsciiTableStatisticRenderer, SJFetcher, HHFetcher, TOP_LANGUAGES
 
 MOSCOW_CITY_CODE = {
     'hh': 1,
@@ -28,11 +12,13 @@ MOSCOW_CITY_CODE = {
 
 SUPERJOB_PROGRAMMER_CATALOGUE = 48
 
+# https://github.com/hhru/api/blob/master/docs/vacancies.md
 STATISTICS_FOR_MOSCOW_QUERY_HH = {'area': MOSCOW_CITY_CODE['hh'],
                                   'date_from': (datetime.datetime.now() - datetime.timedelta(days=30)).strftime(
                                       '%Y-%m-%d'),
                                   'date_to': datetime.datetime.now().strftime('%Y-%m-%d')}
 
+# https://api.superjob.ru/#search_vacanices
 STATISTICS_FOR_MOSCOW_QUERY_SUPERJOB = {'town': MOSCOW_CITY_CODE['sj'],
                                         'date_published_from': int(
                                             (datetime.datetime.now() - datetime.timedelta(days=30)).timestamp()),
@@ -50,7 +36,6 @@ def calculate_hh(limit_langs=None, pages_to_process=None, application_name=None)
 
 def calculate_sj(limit_langs=None, pages_to_process=None, application_name=None):
     sjf = SJFetcher(STATISTICS_FOR_MOSCOW_QUERY_SUPERJOB, app_name=application_name,
-                    search_parameter_name='keyword',
                     pages_to_process=pages_to_process)
     sjf.calculate_statistics_languages(TOP_LANGUAGES[:limit_langs])
     AsciiTableStatisticRenderer('SJ').render(sjf.statistics)
@@ -75,4 +60,4 @@ if __name__ == '__main__':
         limit_langs, pages_to_process = args.lang_limit, args.pages
 
     calculate_hh(limit_langs, pages_to_process, args.name)
-    calculate_sj(limit_langs, pages_to_process + 2, args.name)
+    calculate_sj(limit_langs, pages_to_process, args.name)
--- salary_extractor_lib.py
+++ salary_extractor_lib.py
@@ -8,16 +8,34 @@ import abc
 from terminaltables import AsciiTable
 import requests
 
-APP_NAME_DEFAULT = 'DVMNTutorialApp/1.0 (rk.inwork@gmail.com)'
+SUPERJOB_SEARCH_PARAMETER_NAME = 'keyword'
+
+TOP_LANGUAGES = ['JavaScript',
+                 'Python',
+                 'Java',
+                 'C++',
+                 'C',
+                 'PHP',
+                 'C#',
+                 'Shell',
+                 'Go',
+                 'TypeScript',
+                 'Ruby',
+                 'Objective-C',
+                 'Swift',
+                 'Kotlin',
+                 '1С']
 
 
-class BaseStatisticRenderer(metaclass=ABCMeta):
+
+
+class AbstractBaseStatisticRenderer(metaclass=ABCMeta):
     @abc.abstractmethod
     def render(self, statistics):
         pass
 
 
-class AsciiTableStatisticRenderer(BaseStatisticRenderer):
+class AsciiTableStatisticRenderer(AbstractBaseStatisticRenderer):
     _default_table_headers = (('key', 'Язык программирования'),
                               ('vacancies_found', 'Вакансий найдено'),
                               ('average_salary', 'Средняя зарплата'),
@@ -48,7 +66,7 @@ class AsciiTableStatisticRenderer(BaseStatisticRenderer):
         print(table.table)
 
 
-class StatisticsFetcher(metaclass=ABCMeta):
+class BaseStatisticsFetcher(metaclass=ABCMeta):
     _statistics = {}
 
     def __init__(self, api_config, search_parameter_name='text', pages_to_process=None):
@@ -56,23 +74,23 @@ class StatisticsFetcher(metaclass=ABCMeta):
         self.search_parameter_name = search_parameter_name
         self.pages_to_process = pages_to_process
 
+    @property
+    def statistics(self):
+        return self._statistics
+
     def calculate_statistics_languages(self, languages):
         self._statistics = {}
         for language in languages:
             self.api_config[self.search_parameter_name] = f"программист {language}"
-            self._statistics[language] = self.get_vacancy_statistics()
+            self._statistics[language] = self._get_vacancy_statistics(self.api_config)
             time.sleep(0.5)
 
         print('\r', end='', flush=True)
 
-    @property
-    def raw_statistics(self):
-        return self._statistics
-
-    def get_vacancy_statistics(self):
+    def _get_vacancy_statistics(self, api_config):
         all_vacancies_salaries = []
-        for vacancy in self.fetch_vacancies():
-            all_vacancies_salaries.append(self.predict_rub_salary(vacancy))
+        for salary in self._fetch_vacancies_salaries(api_config):
+            all_vacancies_salaries.append(self._predict_salary_common(*salary))
 
         salaries = [salary for salary in all_vacancies_salaries if salary is not None]
 
@@ -82,12 +100,17 @@ class StatisticsFetcher(metaclass=ABCMeta):
             'vacancies_processed': len(salaries)
         }
 
-    @abc.abstractmethod
-    def fetch_vacancies(self):
-        pass
+    @staticmethod
+    def _get_application_name(app_name):
+        if app_name:
+            return app_name
+        elif os.getenv('DVMN_APP_NAME'):
+            return os.getenv('DVMN_APP_NAME')
+        else:
+            raise Exception("Name of the application hasn't been set")
 
     @staticmethod
-    def predict_salary_common(salary_from, salary_to):
+    def _predict_salary_common(salary_from, salary_to):
         if all((salary_from, salary_to)):
             return mean([salary_from, salary_to])
         if salary_to:
@@ -98,43 +121,43 @@ class StatisticsFetcher(metaclass=ABCMeta):
         return None
 
     @abc.abstractmethod
-    def predict_rub_salary(self, vacancy):
-        pass
-
-    @property
-    def statistics(self):
-        return self._statistics
+    def _fetch_vacancies_salaries(self, api_config):
+        """This method should return min and max salary, unknown value you should substitute with None"""
+        raise NotImplementedError
 
 
-class HHFetcher(StatisticsFetcher):
+class HHFetcher(BaseStatisticsFetcher):
     hh_host = 'https://api.hh.ru/'
 
-    def __init__(self, api_config, app_name=None, search_parameter_name='text', **kwargs):
+    def __init__(self, api_config, app_name=None, **kwargs):
         self.headers = {
-            'User-Agent': app_name or os.getenv('DVMN_APP_NAME', APP_NAME_DEFAULT)
+            'User-Agent': self._get_application_name(app_name)
         }
-        super().__init__(api_config, search_parameter_name, **kwargs)
+        super().__init__(api_config, **kwargs)
 
-    def predict_rub_salary(self, vacancy):
-        salary_description = vacancy.get('salary', None)
-        if not salary_description:
-            return None
-        if salary_description['currency'] != 'RUR':
-            return None
-
-        return self.predict_salary_common(salary_description['from'], salary_description['to'])
-
-    def fetch_vacancies(self):
+    def _fetch_vacancies_salaries(self, api_config):
         endpoint = os.path.join(self.hh_host, 'vacancies')
         for page in itertools.count():
             time.sleep(1)
-            response = requests.get(endpoint, params={**self.api_config, 'page': page}, headers=self.headers)
+            response = requests.get(endpoint, params={**api_config, 'page': page}, headers=self.headers)
             response.raise_for_status()
             response_dict = response.json()
             print("\r" + " " * 100, end="", flush=True)
-            print(f"\r HH process page {response_dict['page']} from {response_dict['pages']}", end="", flush=True)
+            print(
+                f"\r HH process page {response_dict['page']} from {response_dict['pages']} " +
+                f"'{api_config[self.search_parameter_name]}'",
+                end="", flush=True)
 
-            yield from response_dict['items']
+            for vacancy in response_dict['items']:
+                salary_description = vacancy.get('salary', None)
+                if not salary_description:
+                    yield None, None
+                    continue
+                if salary_description['currency'] != 'RUR':
+                    yield None, None
+                    continue
+
+                yield salary_description['from'], salary_description['to']
 
             # for demo purposes
             if self.pages_to_process:
@@ -145,28 +168,32 @@ class HHFetcher(StatisticsFetcher):
                 break
 
 
-class SJFetcher(StatisticsFetcher):
+class SJFetcher(BaseStatisticsFetcher):
     host = 'https://api.superjob.ru/2.0/'
 
-    def __init__(self, api_config, app_name=None, search_parameter_name='text', **kwargs):
+    def __init__(self, api_config, app_name=None, **kwargs):
         self.headers = {
-            'User-Agent': app_name or os.getenv('DVMN_APP_NAME', APP_NAME_DEFAULT),
+            'User-Agent': self._get_application_name(app_name),
             'X-Api-App-Id': os.environ.get("SUPERJOB_API_ID")}
 
-        super().__init__(api_config, search_parameter_name, **kwargs)
+        super().__init__(api_config, SUPERJOB_SEARCH_PARAMETER_NAME, **kwargs)
 
-    def fetch_vacancies(self):
+    def _fetch_vacancies_salaries(self, api_config):
         endpoint = os.path.join(self.host, 'vacancies')
         for page in itertools.count():
-            response = requests.get(endpoint, params={**self.api_config, 'page': page}, headers=self.headers)
+            response = requests.get(endpoint, params={**api_config, 'page': page}, headers=self.headers)
             response.raise_for_status()
 
             response_dict = response.json()
-            print("\r" + " "*100, end="", flush=True)
-            print(f"\r SJ Process link '{self.api_config[self.search_parameter_name]}' page: {page + 1}", end="",
+            print("\r" + " " * 100, end="", flush=True)
+            print(f"\r SJ Process link '{api_config[self.search_parameter_name]}' page: {page + 1}", end="",
                   flush=True)
             time.sleep(1)
-            yield from response_dict['objects']
+            for vacancy in response_dict['objects']:
+                if vacancy['currency'] != 'rub':
+                    yield None, None
+                    continue
+                yield vacancy['payment_from'], vacancy['payment_to']
 
             if self.pages_to_process:
                 if page == self.pages_to_process:
@@ -174,8 +201,3 @@ class SJFetcher(StatisticsFetcher):
 
             if not response_dict['more']:
                 break
-
-    def predict_rub_salary(self, vacancy):
-        if vacancy['currency'] != 'rub':
-            return None
-        return self.predict_salary_common(vacancy['payment_from'], vacancy['payment_to'])
